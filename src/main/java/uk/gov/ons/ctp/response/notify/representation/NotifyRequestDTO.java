9cc7331924d9b4be09b38bd620b2d35b96536739
Emilio Ward <emilioward@hotmail.co.uk>
2018-06-26 14:25:28 +0100

    added styler to codebase

--- src/main/java/uk/gov/ons/ctp/response/notify/representation/NotifyRequestDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/notify/representation/NotifyRequestDTO.java
@@ -1,21 +1,18 @@
 package uk.gov.ons.ctp.response.notify.representation;
 
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
-/**
- * Domain model object
- */
+/** Domain model object */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public abstract class NotifyRequestDTO {
 
-    private String reference;
+  private String reference;
 
-    private Map<String, String> personalisation;
+  private Map<String, String> personalisation;
 }
--- src/main/java/uk/gov/ons/ctp/response/notify/representation/ResponseDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/notify/representation/ResponseDTO.java
@@ -1,28 +1,25 @@
 package uk.gov.ons.ctp.response.notify.representation;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
-/**
- * Domain model object
- */
+/** Domain model object */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class ResponseDTO {
-    private UUID id;
-    private UUID templateId;
+  private UUID id;
+  private UUID templateId;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String fromEmail;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private String fromEmail;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String fromNumber;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private String fromNumber;
 
-    private String reference;
+  private String reference;
 }
